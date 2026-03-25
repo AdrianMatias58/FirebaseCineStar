64656237057266f78df3bbfe12f53146f75a955b
AdrianMatias58 <matihu.919adrian@gmail.com>
2026-03-24 21:13:29 -0500

    Estructurando los archivos

---
+++ js/firebase/firabase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiEdvYmm0wrssq_1C2dOYjA9OeJj33U6w",
+  authDomain: "cinestar-adrian.firebaseapp.com",
+  projectId: "cinestar-adrian",
+  storageBucket: "cinestar-adrian.firebasestorage.app",
+  messagingSenderId: "700109005349",
+  appId: "1:700109005349:web:d32508c6d9e1001d6b95cc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export default db;
