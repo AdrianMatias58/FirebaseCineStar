e800ff8251c509f45d7e42158dbc0723e9b5d407
AdrianMatias58 <matihu.919adrian@gmail.com>
2026-03-24 21:47:14 -0500

    correcion para proteger el firebase

--- js/firebase/firabase.js
+++ js/firebase/firabase.js
@@ -5,14 +5,8 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebas
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCiEdvYmm0wrssq_1C2dOYjA9OeJj33U6w",
-  authDomain: "cinestar-adrian.firebaseapp.com",
-  projectId: "cinestar-adrian",
-  storageBucket: "cinestar-adrian.firebasestorage.app",
-  messagingSenderId: "700109005349",
-  appId: "1:700109005349:web:d32508c6d9e1001d6b95cc",
-};
+//poner la apikeys de firebase projec
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
